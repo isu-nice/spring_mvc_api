872ef7d0557f417d980d394fefb4daaeb6b07423
isu-nice <isu010614@naver.com>
2023-04-12 14:07:02 +0900

    refactor: dto 사용

--- src/main/java/com/codestates/member/MemberController.java
+++ src/main/java/com/codestates/member/MemberController.java
@@ -11,23 +11,27 @@ import java.util.Map;
 @RequestMapping(value = "/v1/members") // produces 설정 제거
 public class MemberController {
 
+    // 회원 정보 등록
     @PostMapping
-    public ResponseEntity postMember(
-            @RequestParam("email") String email,
-            @RequestParam("name") String name,
-            @RequestParam("phone") String phone
-    ) {
-
-        // JSON 문자열 수작업을 Map 객체로 대체
-        Map<String, String> map = new HashMap<>();
-        map.put("email", email);
-        map.put("name", name);
-        map.put("phone", phone);
-
-        // 리턴 값을 ResponseEntity 객체로 변경
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    public ResponseEntity postMember(@RequestBody MemberPostDto memberPostDto) {
+        return new ResponseEntity<>(memberPostDto, HttpStatus.CREATED);
     }
 
+    // 회원 정보 수정
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(
+            @PathVariable("member-id") long memberId,
+            @RequestBody MemberPatchDto memberPatchDto
+    ) {
+        memberPatchDto.setMemberId(memberId);
+        memberPatchDto.setName("홍길동");
+
+        // No need Business logic
+
+        return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
+    }
+
+    // 한명의 회원 정보 조회
     @GetMapping("/{member-id}")
     public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
         System.out.println("# memberId = " + memberId);
@@ -37,6 +41,7 @@ public class MemberController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    // 모든 회원 정보 조회
     @GetMapping
     public ResponseEntity getMembers() {
         System.out.println("# get Members");
@@ -46,4 +51,10 @@ public class MemberController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    // 회원 정보 삭제
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
+        // No need Business logic
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 }
