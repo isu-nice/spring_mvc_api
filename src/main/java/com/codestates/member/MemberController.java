4178003711af5b3bc517f3637645ec4dcefcd82c
isu-nice <isu010614@naver.com>
2023-04-13 13:42:55 +0900

    refactor: DI 수정

--- src/main/java/com/codestates/member/MemberController.java
+++ src/main/java/com/codestates/member/MemberController.java
@@ -7,16 +7,30 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/v1/members") // produces 설정 제거
+@RequestMapping(value = "/v4/members") // produces 설정 제거
 @Validated
 public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
     // 회원 정보 등록
     @PostMapping
     public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto) {
-        return new ResponseEntity<>(memberPostDto, HttpStatus.CREATED);
+
+        Member member = new Member();
+        member.setEmail(memberPostDto.getEmail());
+        member.setName(memberPostDto.getName());
+        member.setPhone(memberPostDto.getPhone());
+
+        Member response = memberService.createMember(member);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     // 회원 정보 수정
@@ -27,36 +41,41 @@ public class MemberController {
     ) {
         memberPatchDto.setMemberId(memberId);
 
-        // No need Business logic
+        Member member = new Member();
+        member.setMemberId(memberPatchDto.getMemberId());
+        member.setName(memberPatchDto.getName());
+        member.setPhone(memberPatchDto.getPhone());
 
-        return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
+        Member response = memberService.updateMember(member);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 한명의 회원 정보 조회
     @GetMapping("/{member-id}")
     public ResponseEntity getMember(@PathVariable("member-id") @Positive long memberId) {
-        System.out.println("# memberId = " + memberId);
-        // not implementation
 
-        // 리턴 값을 ResponseEntity 객체로 변경
-        return new ResponseEntity<>(HttpStatus.OK);
+        Member response = memberService.findMember(memberId);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 모든 회원 정보 조회
     @GetMapping
     public ResponseEntity getMembers() {
-        System.out.println("# get Members");
-        // not implementation
 
-        // 리턴 값을 ResponseEntity 객체로 변경
-        return new ResponseEntity<>(HttpStatus.OK);
+        List<Member> response = memberService.findMembers();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 회원 정보 삭제
     @DeleteMapping("/{member-id}")
     public ResponseEntity deleteMember(@PathVariable("member-id") @Positive long memberId) {
         System.out.println("# deleted memberId = " + memberId);
-        // No need Business logic
+
+        memberService.deleteMember(memberId);
+
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
