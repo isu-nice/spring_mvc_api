de70e9bebf49e4e82a6ee4470c4cf72619412794
isu-nice <isu010614@naver.com>
2023-04-15 12:45:15 +0900

    feat: 멤버 컨트롤러 작성

---
+++ src/main/java/com/codestates/member/controller/MemberController.java
@@ -0,0 +1,95 @@
+package com.codestates.member.controller;
+
+import com.codestates.member.dto.MemberPatchDto;
+import com.codestates.member.dto.MemberPostDto;
+import com.codestates.member.dto.MemberResponseDto;
+import com.codestates.member.entity.Member;
+import com.codestates.member.service.MemberService;
+import com.codestates.member.mapper.MemberMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * - DI 적용
+ * - Mapstruct Mapper 적용
+ */
+@RestController
+@RequestMapping(value = "/v5/members") // produces 설정 제거
+@Validated
+public class MemberController {
+    private final MemberService memberService;
+    private final MemberMapper mapper;
+
+    public MemberController(MemberService memberService, MemberMapper mapper) {
+        this.memberService = memberService;
+        this.mapper = mapper;
+    }
+
+    // 회원 정보 등록
+    @PostMapping
+    public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto) {
+        // mapper를 이용해서 MemberPostDto를 Member로 변환
+        Member member = mapper.memberPostDtoToMember(memberPostDto);
+
+        Member response = memberService.createMember(member);
+
+        // mapper를 이용해서 Member를 MemberResponseDto로 변환
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response),
+                HttpStatus.CREATED);
+    }
+
+    // 회원 정보 수정
+    @PatchMapping("/{member-id}")
+    public ResponseEntity patchMember(
+            @PathVariable("member-id") @Positive long memberId,
+            @Valid @RequestBody MemberPatchDto memberPatchDto
+    ) {
+        memberPatchDto.setMemberId(memberId);
+
+        Member member = mapper.memberPatchDtoToMember(memberPatchDto);
+
+        Member response = memberService.updateMember(member);
+
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response),
+                HttpStatus.OK);
+    }
+
+    // 한명의 회원 정보 조회
+    @GetMapping("/{member-id}")
+    public ResponseEntity getMember(
+            @PathVariable("member-id") @Positive long memberId) {
+
+        Member member = memberService.findMember(memberId);
+        MemberResponseDto response = mapper.memberToMemberResponseDto(member);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    // 모든 회원 정보 조회
+    @GetMapping
+    public ResponseEntity getMembers() {
+
+        List<Member> members = memberService.findMembers();
+        List<MemberResponseDto> response = mapper.membersToMemberResponseDtos(members);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    // 회원 정보 삭제
+    @DeleteMapping("/{member-id}")
+    public ResponseEntity deleteMember(
+            @PathVariable("member-id") @Positive long memberId) {
+
+        System.out.println("# deleted memberId = " + memberId);
+        memberService.deleteMember(memberId);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
