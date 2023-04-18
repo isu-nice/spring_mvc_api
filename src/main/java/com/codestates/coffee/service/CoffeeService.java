b6530f2280472224586af5a47f32dd85fa0e5f4b
isu-nice <isu010614@naver.com>
2023-04-18 14:01:24 +0900

    refactor: 예외 던지도록 변경

--- src/main/java/com/codestates/coffee/service/CoffeeService.java
+++ src/main/java/com/codestates/coffee/service/CoffeeService.java
@@ -1,6 +1,9 @@
 package com.codestates.coffee.service;
 
 import com.codestates.coffee.entity.Coffee;
+import com.codestates.exception.BusinessLogicException;
+import com.codestates.exception.ExceptionCode;
+import com.codestates.order.Order;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,25 +11,27 @@ import java.util.List;
 @Service
 public class CoffeeService {
     public Coffee createCoffee(Coffee coffee) {
-        return coffee;
+        throw new BusinessLogicException(ExceptionCode.NOT_IMPLEMENTATION);
     }
 
     public Coffee updateCoffee(Coffee coffee) {
-        return coffee;
+        throw new BusinessLogicException(ExceptionCode.NOT_IMPLEMENTATION);
     }
 
     public Coffee findCoffee(long coffeeId) {
-        return new Coffee(coffeeId, "아메리카노", "Americano", 2500);
+        throw new BusinessLogicException(ExceptionCode.NOT_IMPLEMENTATION);
     }
 
     public List<Coffee> findCoffees() {
-        return List.of(
-                new Coffee(1L, "아메리카노", "Americano", 2500),
-                new Coffee(2L, "카라멜 라떼", "Caramel Latte", 5000)
-        );
+        throw new BusinessLogicException(ExceptionCode.NOT_IMPLEMENTATION);
     }
 
     public void deleteCoffee(long coffeeId){
+        throw new BusinessLogicException(ExceptionCode.NOT_IMPLEMENTATION);
+    }
 
+    // 주문에 해당하는 커피 정보 조회
+    public List<Coffee> findOrderedCoffees(Order order) {
+        throw new BusinessLogicException(ExceptionCode.NOT_IMPLEMENTATION);
     }
 }
