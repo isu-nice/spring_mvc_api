016753c75872ca9de593256a71d3d9f0c6e4f57d
isu-nice <isu010614@naver.com>
2023-04-18 13:51:35 +0900

    feat: Exception Advice 구현

--- src/main/java/com/codestates/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/codestates/advice/GlobalExceptionAdvice.java
@@ -1,7 +1,10 @@
 package com.codestates.advice;
 
+import com.codestates.exception.BusinessLogicException;
 import com.codestates.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,12 +12,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handlerMethodArgumentNotValidException(
+    public ErrorResponse handleMethodArgumentNotValidException(
             MethodArgumentNotValidException e) {
 
         final ErrorResponse response = ErrorResponse.of(e.getBindingResult());
@@ -24,11 +28,20 @@ public class GlobalExceptionAdvice {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handlerConstraintViolationException(
+    public ErrorResponse handleConstraintViolationException(
             ConstraintViolationException e) {
 
         final ErrorResponse response = ErrorResponse.of(e.getConstraintViolations());
 
         return response;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity handleBusinessLogicException(BusinessLogicException e) {
+
+        final ErrorResponse response = ErrorResponse.of(e.getExceptionCode());
+
+        return new ResponseEntity<>(response,
+                HttpStatus.valueOf(e.getExceptionCode().getStatus()));
     }
 }
