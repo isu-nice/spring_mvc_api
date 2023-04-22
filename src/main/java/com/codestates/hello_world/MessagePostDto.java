1d765ac9d81b844a5c7ffa84a89d17642e340018
isu-nice <isu010614@naver.com>
2023-04-22 13:49:26 +0900

    feat: 메시지 dto 추가

---
+++ src/main/java/com/codestates/hello_world/MessagePostDto.java
@@ -0,0 +1,12 @@
+package com.codestates.hello_world;
+
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+public class MessagePostDto {
+
+    @NotBlank
+    private String message;
+}
