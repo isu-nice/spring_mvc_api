df62c9481d95aea1a782c83bc0a049352eee3df0
isu-nice <isu010614@naver.com>
2023-04-25 15:58:34 +0900

    chore: Money 임시 추가

--- src/main/java/com/codestates/coffee/entity/Coffee.java
+++ src/main/java/com/codestates/coffee/entity/Coffee.java
@@ -7,11 +7,12 @@ import org.springframework.data.annotation.Id;
 @Getter
 @Setter
 public class Coffee {
-
     @Id
     private long coffeeId;
+
     private String korName;
     private String engName;
     private Integer price;
+    // private Money price;
     private String coffeeCode;
 }
