814c3e12164840587c72e89320f12d59d81ef188
isu-nice <isu010614@naver.com>
2023-04-27 21:57:10 +0900

    feat: 학습용 쿼리

--- src/main/java/com/codestates/order/repository/OrderRepository.java
+++ src/main/java/com/codestates/order/repository/OrderRepository.java
@@ -1,8 +1,19 @@
 package com.codestates.order.repository;
 
 import com.codestates.order.entity.Order;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jdbc.repository.query.Query;
+// import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<Order, Long> {
-    
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+   /* @Query("SELECT O.*, OC.QUANTITY, C.* FROM ORDERS O " +
+            "INNER JOIN ORDER_COFFEE OC " +
+            "ON O.ORDER_ID = OC.ORDER_ID " +
+            "INNER JOIN COFFEE C " +
+            "ON OC.COFFEE_ID = C.COFFEE_ID " +
+            "ORDER BY O.ORDER_ID DESC"
+    )
+    List<ReadableOrderCoffee> findAllOrderCoffee();
+    */
 }
