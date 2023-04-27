434aaa2bc134893cdccc2eabb55b85a9e2100436
isu-nice <isu010614@naver.com>
2023-04-27 13:44:50 +0900

    chore: 클래스 삭제

--- src/main/java/com/codestates/hello_world/MessageMapper.java
+++
@@ -1,9 +0,0 @@
-package com.codestates.hello_world;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface MessageMapper {
-    Message messagePostDtoToMessage(MessagePostDto messagePostDto);
-    MessageResponseDto messageToMessageResponseDto(Message message);
-}
