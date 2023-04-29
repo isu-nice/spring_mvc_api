993844a0b40395ffcd0ddcef4912b5e60c9083a8
isu-nice <isu010614@naver.com>
2023-04-29 14:32:23 +0900

    feat: member dto 기능 구현

--- src/main/java/com/codestates/member/dto/MemberPatchDto.java
+++ src/main/java/com/codestates/member/dto/MemberPatchDto.java
@@ -1,16 +1,19 @@
 package com.codestates.member.dto;
 
+import com.codestates.member.entity.Member;
 import com.codestates.validator.NotSpace;
+import lombok.Getter;
 
 import javax.validation.constraints.Pattern;
 
+@Getter
 public class MemberPatchDto {
     // RequestBody에 포함되는 데이터가 아니므로 유효성 검증이 필요하지 않음
     private long memberId;
 
     // 공백 아닌 문자 1개 이상((공백인 문자 0개 또는 1개)(공백이 아닌 문자 1개 이상))
     // -> 마지막 맨 바깥 쪽 괄호 조건이 0개 이상(즉, 있어도 되고 없어도 된다)
-    @Pattern(regexp = "^\\S+(\\s?\\S+)*$", message = "회원 이름은 공백이 아니어야 합니다.")
+    // @Pattern(regexp = "^\\S+(\\s?\\S+)*$", message = "회원 이름은 공백이 아니어야 합니다.")
     @NotSpace(message = "회원 이름은 공백이 아니어야 합니다")
     private String name;
 
@@ -19,28 +22,10 @@ public class MemberPatchDto {
     @NotSpace(message = "휴대폰 번호는 공백이 아니어야 합니다")
     private String phone;
 
-
-    public long getMemberId() {
-        return memberId;
-    }
+    // 회원 상태 값을 사전에 체크하는 validation 만들 수 있음
+    private Member.MemberStatus memberStatus;
 
     public void setMemberId(long memberId) {
         this.memberId = memberId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 }
--- src/main/java/com/codestates/member/dto/MemberResponseDto.java
+++ src/main/java/com/codestates/member/dto/MemberResponseDto.java
@@ -1,17 +1,24 @@
 package com.codestates.member.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.codestates.member.entity.Member;
+import com.codestates.stamp.Stamp;
+import lombok.*;
 
+@Builder
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class MemberResponseDto {
     private long memberId;
     private String email;
     private String name;
     private String phone;
+    private Member.MemberStatus memberStatus;
+    private Stamp stamp;
+
+    public String getMemberStatus() {
+        return memberStatus.getStatus();
+    }
+
+    public int getStamp() {
+        return stamp.getStampCount();
+    }
 }
