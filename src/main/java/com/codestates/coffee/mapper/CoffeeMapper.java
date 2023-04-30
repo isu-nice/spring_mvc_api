79b45ed364fb65a72b8cefb88f9fdf8afdc5e4b2
isu-nice <isu010614@naver.com>
2023-04-30 15:50:23 +0900

    chore: import 정리

--- src/main/java/com/codestates/coffee/mapper/CoffeeMapper.java
+++ src/main/java/com/codestates/coffee/mapper/CoffeeMapper.java
@@ -5,7 +5,6 @@ import com.codestates.coffee.dto.CoffeePostDto;
 import com.codestates.coffee.dto.CoffeeResponseDto;
 import com.codestates.coffee.entity.Coffee;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
 
 import java.util.List;
 
