5ddb52174cfc84087e2e78a1dc898202d322fb54
isu-nice <isu010614@naver.com>
2023-04-30 15:53:52 +0900

    refactor: 쿼리 수정

--- src/main/java/com/codestates/coffee/repository/CoffeeRepository.java
+++ src/main/java/com/codestates/coffee/repository/CoffeeRepository.java
@@ -1,14 +1,17 @@
 package com.codestates.coffee.repository;
 
 import com.codestates.coffee.entity.Coffee;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
+// import org.springframework.data.jdbc.repository.query.Query;
+// import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface CoffeeRepository extends CrudRepository<Coffee, Long> {
+public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
     Optional<Coffee> findByCoffeeCode(String coffeeCode);
 
-    @Query("SELECT * FROM COFFEE WHERE COFFEE_ID = :coffeeId")
+    // @Query("SELECT * FROM COFFEE WHERE COFFEE_ID = :coffeeId")
+    @Query(value = "SELECT c FROM Coffee c WHERE c.coffeeId = :coffeeId") // JPQL
     Optional<Coffee> findByCoffee(Long coffeeId);
 }
