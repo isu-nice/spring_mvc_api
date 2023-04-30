abeb1089c6e03eb101d5bb56acf2fe1a92c61d6e
isu-nice <isu010614@naver.com>
2023-04-30 15:56:09 +0900

    feat: 테이블 매핑 추가

--- src/main/java/com/codestates/member/entity/Member.java
+++ src/main/java/com/codestates/member/entity/Member.java
@@ -1,16 +1,78 @@
 package com.codestates.member.entity;
 
+import com.codestates.audit.Auditable;
+import com.codestates.order.entity.Order;
+import com.codestates.stamp.Stamp;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
 @Getter
 @Setter
-public class Member {
-
+@Entity
+public class Member extends Auditable {
     @Id
-    private long memberId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false, updatable = false, unique = true)
     private String email;
+
+    @Column(length = 100, nullable = false)
     private String name;
+
+    @Column(length = 13, nullable = false, unique = true)
     private String phone;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+    @OneToOne(mappedBy = "member", cascade = CascadeType.PERSIST)
+    private Stamp stamp;
+
+    public Member(String email) {
+        this.email = email;
+    }
+
+    public Member(String email, String name, String phone) {
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public void setOrder(Order order) {
+        orders.add(order);
+        if (order.getMember() != this) {
+            order.setMember(this);
+        }
+    }
+
+    public void setStamp(Stamp stamp) {
+        this.stamp = stamp;
+        if (stamp.getMember() != this) {
+            stamp.setMember(this);
+        }
+    }
+
+    public enum MemberStatus {
+        MEMBER_ACTIVE("활동중"),
+        MEMBER_SLEEP("휴면 상태"),
+        MEMBER_QUIT("탈퇴 상태");
+
+        @Getter
+        private String status;
+
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
 }
