7ddaefe7396053289138a840ba43c9e7de616dd1
isu-nice <isu010614@naver.com>
2023-05-01 12:14:30 +0900

    refactor: 상속받는 인터페이스 변경

--- src/main/java/com/codestates/member/repository/MemberRepository.java
+++ src/main/java/com/codestates/member/repository/MemberRepository.java
@@ -1,10 +1,11 @@
 package com.codestates.member.repository;
 
 import com.codestates.member.entity.Member;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 }
