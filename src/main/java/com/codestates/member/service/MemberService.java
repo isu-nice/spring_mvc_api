31ef62cc1fe22a2076a4f25a7107ca2a0be1fb10
isu-nice <isu010614@naver.com>
2023-05-01 12:14:53 +0900

    refactor: 페이지네이션 적용

--- src/main/java/com/codestates/member/service/MemberService.java
+++ src/main/java/com/codestates/member/service/MemberService.java
@@ -2,52 +2,74 @@ package com.codestates.member.service;
 
 import com.codestates.exception.BusinessLogicException;
 import com.codestates.exception.ExceptionCode;
+import com.codestates.helper.event.MemberRegistrationApplicationEvent;
 import com.codestates.member.entity.Member;
 import com.codestates.member.repository.MemberRepository;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class MemberService {
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+    private final ApplicationEventPublisher publisher;
 
     // 리포지토리 DI
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository,
+                         ApplicationEventPublisher publisher) {
         this.memberRepository = memberRepository;
+        this.publisher = publisher;
     }
 
     public Member createMember(Member member) {
         // 이미 등록된 이메일인지 검증
         verifyExistsEmail(member.getEmail());
+        Member savedMember = memberRepository.save(member);
 
+        // 회원 저장 이벤트 생성
+        publisher.publishEvent(new MemberRegistrationApplicationEvent(this, savedMember));
         // 회원 정보 저장
         return memberRepository.save(member);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
     public Member updateMember(Member member) {
         // 존재하는 회원인지 검증
         Member findMember = findVerifiedMember(member.getMemberId());
 
         // 이름 정보와 휴대폰 번호 정보 업데이트
+        // 회원 정보 일부만 변경할 수도 있으므로 Optional.ofNullable()을 이용해서 null 허용
+        // null 이더라도 NullPointerException 발생하지 않음
         Optional.ofNullable(member.getName())
                 .ifPresent(name -> findMember.setName(name));
         Optional.ofNullable(member.getPhone())
                 .ifPresent(findMember::setPhone);
+        Optional.ofNullable(member.getMemberStatus())
+                .ifPresent(findMember::setMemberStatus);
 
         // 회원 정보 업데이트
         return memberRepository.save(findMember);
     }
 
     // 특정 회원 정보 조회
+    @Transactional(readOnly = true) // 조회 메서드는 readOnly 로 설정
     public Member findMember(long memberId) {
         return findVerifiedMember(memberId);
     }
 
     // 모든 회원 정보 조회
-    public List<Member> findMembers() {
-        return (List<Member>) memberRepository.findAll();
+    // 페이지네이션 적용
+    public Page<Member> findMembers(int page, int size) {
+        return memberRepository.findAll(PageRequest.of(page, size,
+                Sort.by("memberId").descending()));
     }
 
     // 특정 회원 정보 삭제
@@ -58,9 +80,12 @@ public class MemberService {
     }
 
     // 이미 존재하는 회원인지 검증
+    @Transactional(readOnly = true)
     public Member findVerifiedMember(long memberId) {
         Optional<Member> optionalMember =
                 memberRepository.findById(memberId);
+
+        // optionalMember 객체가 null 이면 예외를 던짐
         Member findMember =
                 optionalMember.orElseThrow(() ->
                         new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
@@ -75,6 +100,4 @@ public class MemberService {
             throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
         }
     }
-
-
 }
