cd45905be88c892b86dce8bc44a9b264d2fa67d9
isu-nice <isu010614@naver.com>
2023-05-01 12:13:26 +0900

    refactor: dto 리팩터링에 따른 mapper 수정

--- src/main/java/com/codestates/member/mapper/MemberMapper.java
+++ src/main/java/com/codestates/member/mapper/MemberMapper.java
@@ -1,17 +1,15 @@
 package com.codestates.member.mapper;
 
+import com.codestates.member.dto.MemberDto;
 import com.codestates.member.entity.Member;
-import com.codestates.member.dto.MemberPatchDto;
-import com.codestates.member.dto.MemberPostDto;
-import com.codestates.member.dto.MemberResponseDto;
 import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring") // 스프링의 빈으로 등록됨
+@Mapper(componentModel = "spring")
 public interface MemberMapper {
-    Member memberPostDtoToMember(MemberPostDto memberPostDto);
-    Member memberPatchDtoToMember(MemberPatchDto memberPatchDto);
-    MemberResponseDto memberToMemberResponseDto(Member member);
-    List<MemberResponseDto> membersToMemberResponseDtos(List<Member> members);
+    Member memberPostToMember(MemberDto.Post requestBody);
+    Member memberPatchToMember(MemberDto.Patch requestBody);
+    MemberDto.response memberToMemberResponse(Member member);
+    List<MemberDto.response> membersToMemberResponses(List<Member> members);
 }
