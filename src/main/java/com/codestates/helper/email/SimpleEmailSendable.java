22d567c40c818bcdfa64f8f0fb085221be512717
isu-nice <isu010614@naver.com>
2023-05-08 16:33:36 +0900

    feat: 이메일 센더 구성

---
+++ src/main/java/com/codestates/helper/email/SimpleEmailSendable.java
@@ -0,0 +1,8 @@
+package com.codestates.helper.email;
+
+public class SimpleEmailSendable implements EmailSendable{
+    @Override
+    public void send(String message) throws InterruptedException {
+        // 간단한 문자열 형태의 이메일 구현에 대한 로직을 구성할 수 있다.
+    }
+}
---
+++ src/main/java/com/codestates/helper/email/TemplateEmailSendable.java
@@ -0,0 +1,8 @@
+package com.codestates.helper.email;
+
+public class TemplateEmailSendable implements EmailSendable{
+    @Override
+    public void send(String message) throws InterruptedException {
+        // 템플릿을 사용한 이메일을 보낼 수 있다.
+    }
+}
