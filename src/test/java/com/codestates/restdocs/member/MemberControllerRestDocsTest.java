9b6980b1f615bedac6db1239a5a6ea5dc0433832
isu-nice <isu010614@naver.com>
2023-05-12 09:30:07 +0900

    test: restDocs 생성용 테스트 코드

--- src/test/java/com/codestates/restdocs/member/MemberControllerRestDocsTest.java
+++ src/test/java/com/codestates/restdocs/member/MemberControllerRestDocsTest.java
@@ -1,5 +1,6 @@
 package com.codestates.restdocs.member;
 
+import com.codestates.helper.StubData;
 import com.codestates.member.controller.MemberController;
 import com.codestates.member.dto.MemberDto;
 import com.codestates.member.entity.Member;
@@ -7,36 +8,45 @@ import com.codestates.member.mapper.MemberMapper;
 import com.codestates.member.service.MemberService;
 import com.codestates.stamp.Stamp;
 import com.google.gson.Gson;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static com.codestates.util.ApiDocumentUtils.getRequestPreProcessor;
 import static com.codestates.util.ApiDocumentUtils.getResponsePreProcessor;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 
 @WebMvcTest(MemberController.class) // controller 테스트 전용 에너테이션
 @MockBean(JpaMetamodelMappingContext.class) // JPA에서 사용하는 Bean을 Mock객체로 주입
@@ -175,5 +185,145 @@ public class MemberControllerRestDocsTest {
                                 )
                         )
                 ));
+    }
+
+    @Test
+    public void getMemberTest() throws Exception {
+        // given
+        long memberId = 1L;
+        MemberDto.Response response = new MemberDto.Response(1L,
+                "hgd@gmail.com",
+                "홍길동",
+                "010-1111-1111",
+                Member.MemberStatus.MEMBER_ACTIVE,
+                new Stamp());
+        given(memberService.findMember(Mockito.anyLong())).willReturn(new Member());
+        given(mapper.memberToMemberResponse(Mockito.any(Member.class))).willReturn(response);
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                get("/v11/members/{member-id}", memberId)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.memberId").value(memberId))
+                .andExpect(jsonPath("$.data.name").value(response.getName()))
+                .andDo(
+                        document("get-member",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                pathParameters(
+                                        List.of(parameterWithName("member-id").description("회원 식별자 ID"))
+                                ),
+                                responseFields(
+                                        List.of(
+                                                fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터").optional(),
+                                                fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                                fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
+                                                fieldWithPath("data.name").type(JsonFieldType.STRING).description("이름"),
+                                                fieldWithPath("data.phone").type(JsonFieldType.STRING).description("휴대폰 번호"),
+                                                fieldWithPath("data.memberStatus").type(JsonFieldType.STRING)
+                                                        .description("회원 상태: MEMBER_ACTIVE(활동중) / MEMBER_SLEEP(휴면 계정) / MEMBER_QUIT(탈퇴)"),
+                                                fieldWithPath("data.stamp").type(JsonFieldType.NUMBER).description("스탬프 갯수")
+                                        )
+                                )
+                        ));
+    }
+
+    @Test
+    public void getMembersTest() throws Exception {
+        // given
+        String page = "1";
+        String size = "10";
+
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+        queryParams.add("page", page);
+        queryParams.add("size", size);
+
+        Member member1 = new Member("hgd1@gmail.com", "홍길동1", "010-1111-1111");
+        member1.setMemberStatus(Member.MemberStatus.MEMBER_ACTIVE);
+        member1.setStamp(new Stamp());
+
+        Member member2 = new Member("hgd2@gmail.com", "홍길동2", "010-2222-2222");
+        member2.setMemberStatus(Member.MemberStatus.MEMBER_ACTIVE);
+        member2.setStamp(new Stamp());
+
+        Page<Member> members = new PageImpl<>(List.of(member1, member2),
+                PageRequest.of(0, 10, Sort.by("memberId").descending()), 2);
+        List<MemberDto.Response> responses = StubData.MockMember.getMultiResponseBody();
+
+        // Stubbing
+        given(memberService.findMembers(Mockito.anyInt(), Mockito.anyInt())).willReturn(members);
+        given(mapper.membersToMemberResponses(Mockito.anyList())).willReturn(responses);
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                get("/v11/members")
+                        .params(queryParams)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        MvcResult result =
+                actions
+                        .andExpect(status().isOk())
+                        .andDo(
+                                document(
+                                        "get-members",
+                                        preprocessRequest(prettyPrint()),
+                                        preprocessResponse(prettyPrint()),
+                                        requestParameters(
+                                                List.of(
+                                                        parameterWithName("page").description("Page 번호"),
+                                                        parameterWithName("size").description("Page Size")
+                                                )
+                                        ),
+                                        responseFields(
+                                                List.of(
+                                                        fieldWithPath("data").type(JsonFieldType.ARRAY).description("결과 데이터").optional(),
+                                                        fieldWithPath("data[].memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                                        fieldWithPath("data[].email").type(JsonFieldType.STRING).description("이메일"),
+                                                        fieldWithPath("data[].name").type(JsonFieldType.STRING).description("이름"),
+                                                        fieldWithPath("data[].phone").type(JsonFieldType.STRING).description("휴대폰 번호"),
+                                                        fieldWithPath("data[].memberStatus").type(JsonFieldType.STRING)
+                                                                .description("회원 상태: MEMBER_ACTIVE(활동중) / MEMBER_SLEEP(휴면 계정) / MEMBER_QUIT(탈퇴)"),
+                                                        fieldWithPath("data[].stamp").type(JsonFieldType.NUMBER).description("스탬프 갯수"),
+                                                        fieldWithPath("pageInfo").type(JsonFieldType.OBJECT).description("페이지 정보"),
+                                                        fieldWithPath("pageInfo.page").type(JsonFieldType.NUMBER).description("페이지 번호"),
+                                                        fieldWithPath("pageInfo.size").type(JsonFieldType.NUMBER).description("페이지 사이즈"),
+                                                        fieldWithPath("pageInfo.totalElements").type(JsonFieldType.NUMBER).description("전체 건 수"),
+                                                        fieldWithPath("pageInfo.totalPages").type(JsonFieldType.NUMBER).description("전체 페이지 수")
+                                                )
+                                        )
+                                )
+                        )
+                        .andReturn();
+
+        List list = JsonPath.parse(result.getResponse().getContentAsString()).read("$.data");
+        assertThat(list.size(), is(2));
+    }
+
+    @Test
+    public void deleteMemberTest() throws Exception {
+        // given
+        long memberId = 1L;
+        doNothing().when(memberService).deleteMember(Mockito.anyLong());
+
+        // when
+        ResultActions actions = mockMvc.perform(
+                delete("/v11/members/{member-id}", memberId));
+        // then
+        actions.andExpect(status().isNoContent())
+                .andDo(
+                        document(
+                                "delete-member",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                pathParameters(
+                                        Arrays.asList(parameterWithName("member-id").description("회원 식별자 ID"))
+                                )
+                        )
+                );
     }
 }
