98ae9a73f24ace534f43ce77d5dbb39766db921e
isu-nice <isu010614@naver.com>
2023-05-24 16:17:38 +0900

    chore: 상속 제거

--- src/main/java/com/codestates/helper/event/MemberRegistrationApplicationEvent.java
+++ src/main/java/com/codestates/helper/event/MemberRegistrationApplicationEvent.java
@@ -2,14 +2,12 @@ package com.codestates.helper.event;
 
 import com.codestates.member.entity.Member;
 import lombok.Getter;
-import org.springframework.context.ApplicationEvent;
 
 @Getter
-public class MemberRegistrationApplicationEvent extends ApplicationEvent {
+public class MemberRegistrationApplicationEvent {
     private Member member;
 
-    public MemberRegistrationApplicationEvent(Object source, Member member) {
-        super(source);
+    public MemberRegistrationApplicationEvent(Member member) {
         this.member = member;
     }
 }
